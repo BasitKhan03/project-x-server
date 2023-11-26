9129d96c965038ce1a6564e6e3a6921370652478
Basit Khan <basitkhan1010.abk@gmail.com>
2023-11-27 03:50:50 +0500

    Add files via upload

---
+++ data/events/leaves/insertLeaveApplication.sql
@@ -0,0 +1,43 @@
+INSERT INTO LeaveApplications 
+    (EmployeeId, 
+    LeaveTypeId, 
+    FromDate, 
+    ToDate, 
+    DaysCount, 
+    LeaveReasonId, 
+    ReasonDetail, 
+    ApproverId, 
+    ApproverDetail,
+    LeaveStatusId, 
+    LeaveStatusHODId, 
+    LeaveStatusPRNId,
+    LeaveStatusHRId, 
+    LeaveStatusVCId, 
+    LeaveValidityId, 
+    LeaveValidityRemarks, 
+    AttachmentFilePath, 
+    IsActive,
+    CreateDateTime) 
+VALUES 
+    (@EmployeeId, 
+    @LeaveTypeId, 
+    @FromDate, 
+    @ToDate, 
+    DATEDIFF(day, @FromDate, @ToDate) + 1,
+    @LeaveReasonId, 
+    @ReasonDetail, 
+    CASE
+        WHEN (SELECT TOP 1 supervisor_id FROM Groups WHERE GroupId = (SELECT Group_GroupId FROM Employees WHERE EmployeeId = @EmployeeId)) = @EmployeeId THEN '000000'
+        ELSE (SELECT TOP 1 supervisor_id FROM Groups WHERE GroupId = (SELECT Group_GroupId FROM Employees WHERE EmployeeId = @EmployeeId))
+    END, 
+    NULL, 
+    1, 
+    1, 
+    1, 
+    1, 
+    1, 
+    1, 
+    '--',
+    @Attachment, 
+    1,
+    GETDATE())
